6b9abec9445a305fd7e04634c33b87b3b5e307e0
David Shere <sheredlk@gmail.com>
2021-09-12 09:09:36 -0700

    updates

--- airquality/__init__.py
+++ airquality/__init__.py
@@ -2,8 +2,6 @@ import os
 
 from flask import Flask
 
-from airquality.sensor import get_pmi_result
-
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -25,15 +23,11 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simplepage that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    from . import db
+    db.init_app(app)
 
-    @app.route('/')
-    def get_pmi():
-        two_point_five, ten = get_pmi_result()
-        return f"PMI 2.5: {two_point_five}, PMI 10: {ten}\n"
+    from . import readings
+    app.register_blueprint(readings.bp)
 
     return app
 
